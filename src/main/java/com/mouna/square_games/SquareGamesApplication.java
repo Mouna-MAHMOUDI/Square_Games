8ca2b2f0e7dd3ae3f9c6a605802b0d1a1b61bb9e
Mouna.MAHMOUDI <mahmoudi_mouna@yahoo.fr>
2026-09-10 10:18:54 +0200

    first commit

---
+++ src/main/java/com/mouna/square_games/SquareGamesApplication.java
@@ -0,0 +1,13 @@
+package com.mouna.square_games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquareGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquareGamesApplication.class, args);
+	}
+
+}
