43ff6ade15c6777109f48f0a61a04c3871c910e4
Mouna-MAHMOUDI <mouna.mahmoudi@le-campus-numerique.fr>
2026-09-17 21:11:29 +0200

    JPA changes

--- src/main/java/com/mouna/square_games/entity/GameTokenEntity.java
+++ src/main/java/com/mouna/square_games/entity/GameTokenEntity.java
@@ -1,11 +1,14 @@
 package com.mouna.square_games.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class GameTokenEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
     public String ownerId;
     public String name;
